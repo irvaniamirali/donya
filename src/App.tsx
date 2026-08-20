a0051d5d5a4352623ddfd9a094c75c172d0a0eb6
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2026-08-20 08:13:47 +0330

    feat: bootstrap Donya with Tauri

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/app.css";
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Donya</h1>
+      <p>Your music, beautifully organized.</p>
+    </main>
+  );
+}
+
+export default App;
